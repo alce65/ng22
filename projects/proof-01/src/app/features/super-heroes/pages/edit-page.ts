539652781327a898f85388ae694c89fb252e268c
Alejandro Cerezo <alce65@hotmail.es>
2026-06-20 08:54:36 +0200

    [Proof-01] Add control errors with NotFoundHero component

--- projects/proof-01/src/app/features/super-heroes/pages/edit-page.ts
+++ projects/proof-01/src/app/features/super-heroes/pages/edit-page.ts
@@ -1,36 +1,40 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { Card } from '../../../core/components/card/card';
 import { HeroForm } from '../components/hero-form/hero-form';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HeroesState } from '../services/heroes-state';
 import { JsonPipe } from '@angular/common';
+import { NotFoundHero } from '../components/not-found-hero/not-found-hero';
 
 @Component({
   selector: 'alc-edit-page',
-  imports: [Card, HeroForm, JsonPipe],
+  imports: [Card, HeroForm, NotFoundHero, JsonPipe],
   template: `
     <header>
       <alc-card>
         <h2>Editar un Super Héroe</h2>
       </alc-card>
     </header>
-    <alc-hero-form
-    [initialHero]="hero()"
-    (addHeroEvent)="updateHero($event)" />
+    @if (isNullHero()) {
+      <alc-not-found-hero />
+    } @else {
+      <alc-hero-form [initialHero]="hero()" (addHeroEvent)="updateHero($event)" />
       <pre>{{ hero() | json }}</pre>
+    }
   `,
   styles: ``,
 })
 export default class EditPage {
-  readonly router = inject(Router);
-  protected activateRoute = inject(ActivatedRoute);
-  protected hero = signal(this.activateRoute.snapshot.data['superHero']);
+  readonly #router = inject(Router);
+  readonly #activateRoute = inject(ActivatedRoute);
+  readonly #heroState = inject(HeroesState);
 
-  readonly #heroService = inject(HeroesState);
+  protected readonly hero = signal(this.#activateRoute.snapshot.data['superHero']);
+  protected readonly isNullHero = computed(() => this.#heroState.isNullHero(this.hero()));
 
   updateHero(hero: any): void {
     console.log('Updating hero:', hero);
-    this.#heroService.update(hero);
-    this.router.navigate(['/super-heroes']);
+    this.#heroState.update(hero);
+    this.#router.navigate(['/super-heroes']);
   }
 }
