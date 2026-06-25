7d9bc2a28fc1762c73f07e7b64395c05f7df51a7
Alejandro Cerezo <alce65@hotmail.es>
2026-06-25 12:48:56 +0200

    [Demo 03] Update login signal form with minor refactors

--- projects/demo-03/src/app/features/auth/components/login-form-signals/login-form-signals.ts
+++ projects/demo-03/src/app/features/auth/components/login-form-signals/login-form-signals.ts
@@ -1,9 +1,19 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
-import { email, form, FormField, FormRoot, minLength, required } from '@angular/forms/signals';
+import {
+  email,
+  FieldState,
+  FieldTree,
+  form,
+  FormField,
+  FormRoot,
+  minLength,
+  required,
+  SchemaPathTree,
+} from '@angular/forms/signals';
 import { Router } from '@angular/router';
 
-interface LoginForm {
+interface LoginModel {
   email: string;
   password: string;
   rememberMe: boolean;
@@ -34,36 +44,83 @@ interface LoginForm {
         <span>Remember me</span>
       </label>
       <div class="form-control">
-        <button type="submit" class="btn btn-primary" [disabled]="loginForm().invalid()">Login</button>
+        <button type="submit" class="btn btn-primary" [disabled]="loginForm().invalid()">
+          Login
+        </button>
       </div>
     </form>
-    <pre>{{ loginForm().value() | json }}</pre>
+    <div aria-label="Form Value" class="form-value">
+      <p>Valor del formulario</p>
+      <pre>{{ loginForm().value() | json }}</pre>
+      <p>Valor de errores activos del formulario</p>
+      <pre>{{ loginForm().errorSummary() | json }}</pre>
+      <p>Errores activos del formulario</p>
+      <ul class="error">
+        @for (error of loginForm().errorSummary(); track $index) {
+          <li>{{ error.message }}</li>
+        }
+      </ul>
+    </div>
   `,
   styleUrls: ['../forms.css'],
-  styles: ``,
+  styles: `
+    ul.error {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+    }
+    .form-value {
+      margin-top: 2rem;
+      padding: 1rem;
+      border: 1px solid #ccc;
+      background-color: #f9f9f9;
+    }
+  `,
 })
 export class LoginFormSignals {
   readonly #router = inject(Router);
-  readonly #loginFormInitialState: LoginForm = {
+  readonly #loginModelInitialState: LoginModel = {
     email: '',
     password: '',
     rememberMe: false,
   };
 
-  // Signal representing the form state
-  readonly #loginFormState = signal<LoginForm>(this.#loginFormInitialState);
-  // Sin valifdaciones
-  // protected readonly loginForm = form(this.#loginFormState)
+  // Signal representing the form model
+  readonly #loginModel = signal<LoginModel>(this.#loginModelInitialState);
+
+  // Sin validaciones
+  // protected readonly loginForm = form(this.#loginModel)
+
   // Añadiendo validaciones
-  protected readonly loginForm = form(this.#loginFormState, (path) => {
+  // protected readonly loginForm = form(this.#loginModel, (path) => {
+  //   required(path.email, { message: 'El email es obligatorio' });
+  //   email(path.email, { message: 'El email debe ser una dirección de correo válida' });
+  //   required(path.password, { message: 'La contraseña es obligatoria' });
+  //   minLength(path.password, 6, { message: 'La contraseña debe tener al menos 6 caracteres' });
+  // });
+
+  // Schema de validación como propiedad de la clase
+
+  readonly #loginFormSchema = (path:  SchemaPathTree<LoginModel>) => {
     required(path.email, { message: 'El email es obligatorio' });
     email(path.email, { message: 'El email debe ser una dirección de correo válida' });
     required(path.password, { message: 'La contraseña es obligatoria' });
     minLength(path.password, 6, { message: 'La contraseña debe tener al menos 6 caracteres' });
-  });
+  };
+
+  protected readonly loginForm = form(this.#loginModel, this.#loginFormSchema);
+
+  constructor() {
+    const fieldTree: FieldTree<LoginModel> = this.loginForm;
+    const fieldState: FieldState<LoginModel> = fieldTree();
+    const formValue = fieldState.value();
+
+    const fieldEmail: FieldTree<LoginModel['email']> = fieldTree.email;
+    const fieldStateEmail: FieldState<LoginModel['email']> = fieldEmail();
+    const emailValue = fieldStateEmail.value();
+  }
 
   protected submitForm() {
-    console.log('Form submitted');
     if (this.loginForm().valid()) {
       console.log('Form submitted');
       const formData = this.loginForm().value();
